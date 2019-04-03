69fba2896e24590ff49188f3eda1b84cffd599c3
Vibgy <vibgyj@gmail.com>
2019-04-03 21:24:49 +0530

    Coded War logic

--- TransformerBattle/BusinessLayer.cs
+++ TransformerBattle/BusinessLayer.cs
@@ -18,9 +18,61 @@ namespace TransformerBattle.BusinessLayer
 
     public class War : IWar
     {
+        private readonly IBattle battle;
+        public War(IBattle battle)
+        {
+            this.battle = battle;
+        }
+
         public List<Guid> SimulateWar(List<Transformer> transformers)
         {
-            throw new NotImplementedException();
+            if (transformers.Any(t => t.Name.Equals("Optimus")) && transformers.Any(t => t.Name.Equals("Predaking")))
+                return new List<Guid>();
+
+            var autobots = transformers.Where(t => t.Allegiance.Equals(Group.Autobot)).OrderByDescending(t => t.Rank);
+            var decepticons = transformers.Where(t => t.Allegiance.Equals(Group.Decepticon)).OrderByDescending(t => t.Rank);
+
+            var result = new List<Guid>();
+
+            IEnumerable<Transformer> largerGroup;
+            IEnumerable<Transformer> smallerGroup;
+            if (autobots.Count() > decepticons.Count())
+            {
+                largerGroup = autobots;
+                smallerGroup = decepticons;
+            }
+            else
+            {
+                largerGroup = decepticons;
+                smallerGroup = autobots;
+            }
+
+            for(int i = 0; i < largerGroup.Count(); i++)
+            {
+                if (smallerGroup.Count() <= i)
+                {
+                    result.Add(largerGroup.ElementAt(i).Id);
+                }
+                else
+                {
+                    var victor = battle.SimulateBattle(largerGroup.ElementAt(i), smallerGroup.ElementAt(i));
+                    result.Add(victor);
+                }                    
+            }
+            //var autobotsCount = autobots.Count();
+            //var decepticonsCount = decepticons.Count();
+            //var minLenth = autobotsCount < decepticonsCount ? autobotsCount : decepticonsCount;
+
+            
+            //for(int i = 0; i < minLenth; i++)
+            //{
+            //    var victor = battle.SimulateBattle(autobots[i], decepticons[i]);
+            //    result.Add(victor);
+            //}
+
+            //if()
+
+            return result;
         }
     }
 
---
+++ TransformersBattleTests/WarTests.cs
@@ -0,0 +1,81 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TransformerBattle.BusinessLayer;
+using TransformerBattle.DataLayer;
+using Xunit;
+
+namespace TransformersBattleTests
+{
+    public class WarTests
+    {
+        [Fact]
+        public void SimulateWar_Optimus_Predaking()
+        {
+            var war = new War(null);
+
+            var transformers = new List<Transformer>
+            {
+                new Transformer { Name = "Optimus" },
+                new Transformer { Name = "Predaking" }
+            };
+
+            var result = war.SimulateWar(transformers);
+
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void SimulateWar_SplitsByAllegiance()
+        {
+            var transformerA = new Transformer { Id = Guid.NewGuid(), Name = "A1", Allegiance = Group.Autobot };
+            var transformerD = new Transformer { Id = Guid.NewGuid(), Name = "D1", Allegiance = Group.Decepticon };
+            var transformers = new List<Transformer> { transformerA, transformerD };
+
+            var mockBattle = new Mock<IBattle>();
+            var war = new War(mockBattle.Object);
+            mockBattle.Setup(b => b.SimulateBattle(transformerD, transformerA)).Returns(transformerA.Id);
+
+            var victors = war.SimulateWar(transformers);
+
+            Assert.Contains(transformerA.Id, victors);
+        }
+
+        [Fact]
+        public void SimulateWar_UnEvenCount()
+        {
+            var transformerA = new Transformer { Id = Guid.NewGuid(), Name = "A1", Allegiance = Group.Autobot };
+            var transformerA2 = new Transformer { Id = Guid.NewGuid(), Name = "A2", Allegiance = Group.Autobot };
+            var transformerD = new Transformer { Id = Guid.NewGuid(), Name = "D1", Allegiance = Group.Decepticon };
+            var transformers = new List<Transformer> { transformerA, transformerD, transformerA2 };
+
+            var mockBattle = new Mock<IBattle>();
+            var war = new War(mockBattle.Object);
+            mockBattle.Setup(b => b.SimulateBattle(transformerA, transformerD)).Returns(transformerA.Id);
+
+            var victors = war.SimulateWar(transformers);
+
+            Assert.Contains(transformerA.Id, victors);
+            Assert.Contains(transformerA2.Id, victors);
+        }
+
+        [Fact]
+        public void SimulateWar_UnEvenWithRank()
+        {
+            var transformerA = new Transformer { Id = Guid.NewGuid(), Name = "A1", Allegiance = Group.Autobot, Rank = 4 };
+            var transformerA2 = new Transformer { Id = Guid.NewGuid(), Name = "A2", Allegiance = Group.Autobot, Rank = 5 };
+            var transformerD = new Transformer { Id = Guid.NewGuid(), Name = "D1", Allegiance = Group.Decepticon, Rank = 6 };
+            var transformers = new List<Transformer> { transformerA, transformerD, transformerA2 };
+
+            var mockBattle = new Mock<IBattle>();
+            var war = new War(mockBattle.Object);
+            mockBattle.Setup(b => b.SimulateBattle(transformerA2, transformerD)).Returns(transformerD.Id);
+
+            var victors = war.SimulateWar(transformers);
+
+            Assert.Contains(transformerD.Id, victors);
+            Assert.Contains(transformerA.Id, victors);
+        }
+    }
+}
